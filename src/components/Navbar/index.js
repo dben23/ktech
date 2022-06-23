ac8b6a31b663649ddb5041271ef7b1c2b2f8ecf3
dben23 <98925753+dben23@users.noreply.github.com>
2022-06-23 19:52:34 +0200

    the beginnings

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+
+import "./logo.style.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <img
+              src={require("../../images/logo-nb.png")}
+              alt="logo"
+              className="logo"
+            />
+          </NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="domov">Domov</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="onas">O Nás</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="smarthome">Smart Home</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="kontakt">Kontakt</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
